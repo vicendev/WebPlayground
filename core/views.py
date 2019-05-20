0d373e3a8e0ecb9283e881d1af0847cfe1dfc4f0
Vicente Moyano <vicenmoyano.upla@gmail.com>
2019-05-20 18:49:35 -0400

    Curso Udemy Django Parte 3

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.views.generic.base import TemplateView
+from django.shortcuts import render
+
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Mi super web Playground"
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title':"Mi super web Playground"})
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
